e78b9d31173c13f7db181cb0494c8c35543f7dea
kelu0725 <klu2@alumni.cmu.edu>
2021-10-26 13:34:22 -0700

    refactor js

--- App.js
+++ App.js
@@ -1,54 +1,55 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
-const resetButton = document.querySelector('#reset');
+const p1 = {
+  score: 0,
+  display: document.querySelector("#p1Display"),
+  button: document.querySelector("#p1Button"),
+};
+const p2 = {
+  score: 0,
+  display: document.querySelector("#p2Display"),
+  button: document.querySelector("#p2Button"),
+};
+const resetButton = document.querySelector("#reset");
 const winningScoreSelet = document.querySelector("#playto");
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
-p1Button.addEventListener('click', function(){
-    if(!isGameOver){
-        p1Score +=1;
-    }
-   if(p1Score === winningScore){
-       isGameOver=true;
-       p1Display.classList.add('winner');
-       p2Display.classList.add('loser');
-   }
-   p1Display.textContent=p1Score;
-})
 
-p2Button.addEventListener('click', function(){
-    if(!isGameOver){
-        p2Score +=1;
-   
+function addScore(player, opponent) {
+  if (!isGameOver) {
+    player.score += 1;
+    if (player.score === winningScore) {
+      isGameOver = true;
+      player.display.classList.add("has-text-success");
+      opponent.display.classList.add("has-text-danger");
+      player.button.disabled = true;
+      opponent.button.disabled = true;
     }
-    if(p2Score === winningScore){
-        isGameOver=true;
-        p1Display.classList.add('loser');
-        p2Display.classList.add('winner');
-    }
-    p2Display.textContent=p2Score;
-})
-
-
-function reset(){
-    isGameOver=false;
-    p1Score=0;
-    p2Score=0;
-    p1Display.textContent=p1Score;
-    p2Display.textContent=p2Score;
-    p1Display.classList.remove('winner','loser');
-    p2Display.classList.remove('winner','loser');
+    player.display.textContent = player.score;
+  }
 }
 
+//Callback has to be function(), cannot call function directly
+p1.button.addEventListener("click", function () {
+  addScore(p1, p2);
+});
+p2.button.addEventListener("click", function () {
+  addScore(p2, p1);
+});
 
+winningScoreSelet.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
 
-resetButton.addEventListener('click', reset)
-
- winningScoreSelet.addEventListener('change', function(){
-     winningScore = parseInt(this.value);
-     reset();
- })
+function reset() {
+  isGameOver = false;
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.display.classList.remove("has-text-success", "has-text-danger");
+    p.display.textContent = 0;
+    p.button.disabled = false;
+  }
+}
+//here function is reset, not reset()
+resetButton.addEventListener("click", function () {
+  reset();
+});
